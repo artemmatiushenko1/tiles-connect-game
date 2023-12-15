2df381ab977ca63e751ed2679c492baac8f8ba94
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-12-15 22:19:49 +0200

    fix: * use Array.from

--- src/utils/createEmptyMatrix.js
+++ src/utils/createEmptyMatrix.js
@@ -1,10 +1,12 @@
-import { TILES_GRID_SIZE } from '../constants';
+import { TILES_GRID_SIZE, TILE_STATE } from '../constants';
 
 const createEmptyMatrix = (
   rows = TILES_GRID_SIZE.ROWS,
   cols = TILES_GRID_SIZE.COLS
 ) => {
-  return new Array(rows).fill(0).map((row) => new Array(cols).fill(0, 0, cols));
+  return Array.from({ length: rows }, () =>
+    Array.from({ length: cols }, () => TILE_STATE.CLOSED)
+  );
 };
 
 export default createEmptyMatrix;
